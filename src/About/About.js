190679d83a8c4203db9013f3c1289cb8a3b270d0
Jim Talarski <jim.talarski@gmail.com>
2020-09-15 19:14:27 -0500

    base mode is working

---
+++ src/About/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>We are actors worker with actors and we react to things</p>
+      </div>
+    );
+  }
+
+}
+
+export default About;
